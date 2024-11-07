9d9645206ffed1a125664c406cce1406eb203b66
Django Cass <django@dcas.dev>
2024-11-07 15:26:15 +1100

    syft no longer returns an error when it doesn't support the target image type (e.g., helm charts)

--- internal/generators/sbom/syft.go
+++ internal/generators/sbom/syft.go
@@ -8,6 +8,8 @@ import (
 	"github.com/anchore/stereoscope/pkg/image"
 	"github.com/anchore/syft/syft"
 	"github.com/anchore/syft/syft/format/cyclonedxjson"
+	"github.com/anchore/syft/syft/sbom"
+	"github.com/anchore/syft/syft/source"
 	civ1 "github.com/djcass44/ci-tools/internal/api/v1"
 	"log"
 	"os"
@@ -29,15 +31,40 @@ func Execute(ctx context.Context, bctx *civ1.BuildContext, ref, digest string) e
 	sourceOptions.SourceProviderConfig.RegistryOptions = &image.RegistryOptions{
 		Keychain: auth.KeyChain(bctx.Auth()),
 	}
+	var artefact *sbom.SBOM
 	src, err := syft.GetSource(ctx, ref, sourceOptions)
 	if err != nil {
-		return err
+		if !strings.Contains(err.Error(), "unknown layer media type") {
+			return fmt.Errorf("fetching image source: %w", err)
+		}
+		log.Printf("syft failed to generate SBOM for unknown image media type: %v", err)
+		// generate a skeleton SBOM for unsupported media types
+		// similar to what Trivy does
+		artefact = &sbom.SBOM{
+			Artifacts: sbom.Artifacts{},
+			Source: source.Description{
+				ID:      digest,
+				Name:    ref,
+				Version: bctx.Repo.CommitSha,
+				Metadata: map[string]any{
+					"userInput":      ref,
+					"tags":           bctx.Tags,
+					"manifestDigest": "sha256:" + digest,
+				},
+			},
+			Descriptor: sbom.Descriptor{
+				Name:    "ci",
+				Version: "unknown",
+			},
+		}
 	}
 
 	// hand it off to Syft
-	artefact, err := syft.CreateSBOM(ctx, src, syft.DefaultCreateSBOMConfig())
-	if err != nil {
-		return err
+	if artefact == nil {
+		artefact, err = syft.CreateSBOM(ctx, src, syft.DefaultCreateSBOMConfig())
+		if err != nil {
+			return fmt.Errorf("generating SBOM: %w", err)
+		}
 	}
 	// convert the SBOM into a CycloneDX
 	// report.
@@ -47,11 +74,11 @@ func Execute(ctx context.Context, bctx *civ1.BuildContext, ref, digest string) e
 	}
 	var buffer bytes.Buffer
 	if err := encoder.Encode(&buffer, *artefact); err != nil {
-		return err
+		return fmt.Errorf("encoding SBOM: %w", err)
 	}
 	// write to file
 	if err := os.WriteFile(filepath.Join(bctx.Root, outSBOM), buffer.Bytes(), 0644); err != nil {
-		return err
+		return fmt.Errorf("saving SBOM: %w", err)
 	}
 	return nil
 }
--- internal/generators/sbom/syft_test.go
+++ internal/generators/sbom/syft_test.go
@@ -18,7 +18,7 @@ func TestExecute(t *testing.T) {
 		err := sbom.Execute(context.TODO(), &civ1.BuildContext{
 			Root: tmp,
 			Image: civ1.ImageConfig{
-				Name:     " harbor.dcas.dev/docker.io/library/ubuntu",
+				Name:     "harbor.dcas.dev/docker.io/library/ubuntu",
 				Username: "",
 				Password: "",
 			},
@@ -41,7 +41,7 @@ func TestExecute(t *testing.T) {
 		err := sbom.Execute(context.TODO(), &civ1.BuildContext{
 			Root: tmp,
 			Image: civ1.ImageConfig{
-				Name:     " harbor.dcas.dev/docker.io/library/ubuntu",
+				Name:     "harbor.dcas.dev/docker.io/library/ubuntu",
 				Username: "",
 				Password: "",
 			},
@@ -52,6 +52,29 @@ func TestExecute(t *testing.T) {
 				Path:    filepath.Join(tmp, ".cache"),
 			},
 		}, "harbor.dcas.dev/docker.io/library/ubuntu:18.04@sha256:98706f0f213dbd440021993a82d2f70451a73698315370ae8615cc468ac06624", "98706f0f213dbd440021993a82d2f70451a73698315370ae8615cc468ac06624")
+		assert.NoError(t, err)
+
+		data, err := os.ReadFile(filepath.Join(tmp, "sbom.cdx.json"))
+		assert.NoError(t, err)
+		t.Log(string(data))
+	})
+	t.Run("helm chart", func(t *testing.T) {
+		tmp := t.TempDir()
+		require.NoError(t, os.MkdirAll(filepath.Join(tmp, ".cache"), 0750))
+		err := sbom.Execute(context.TODO(), &civ1.BuildContext{
+			Root: tmp,
+			Image: civ1.ImageConfig{
+				Name:     "harbor.dcas.dev/ghcr.io/grafana/helm-charts/grafana:8.5.12",
+				Username: "",
+				Password: "",
+			},
+			Tags:   []string{"latest"},
+			FQTags: []string{"harbor.dcas.dev/ghcr.io/grafana/helm-charts/grafana:8.5.12"},
+			Cache: civ1.BuildCache{
+				Enabled: true,
+				Path:    filepath.Join(tmp, ".cache"),
+			},
+		}, "harbor.dcas.dev/ghcr.io/grafana/helm-charts/grafana:8.5.12", "93c94db36aa18ec8e647c0306e950072ec715aa1f7029337a1d82f6eb08ba212")
 		assert.NoError(t, err)
 
 		data, err := os.ReadFile(filepath.Join(tmp, "sbom.cdx.json"))
